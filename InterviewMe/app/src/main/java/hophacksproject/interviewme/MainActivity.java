3a2c0ae74115e8445176a904e4edce3d33095d83
alyssaschilke <arschilke@loyola.edu>
2019-02-16 08:26:12 -0500

    setupMainActivity

--- InterviewMe/app/src/main/java/hophacksproject/interviewme/MainActivity.java
+++ InterviewMe/app/src/main/java/hophacksproject/interviewme/MainActivity.java
@@ -1,13 +1,27 @@
 package hophacksproject.interviewme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button interviewChat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        interviewChat = findViewById(R.id.chatBtn);
+
+        interviewChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent chatBot = new Intent(getApplicationContext(), chatActivity.class);
+                startActivity(chatBot);
+            }
+        });
     }
+
 }
